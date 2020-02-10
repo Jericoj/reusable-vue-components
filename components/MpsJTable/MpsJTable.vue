b67c30ced9965549c102a1448502d231842d9d25
Jericoj <jericoj2@gmail.com>
2020-02-10 13:08:03 +0100

    JTable component

---
+++ components/MpsJTable/MpsJTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <JTableRow
+      v-for="(values, index) in items"
+      :key="index"
+      :values="values"
+    />
+  </table>
+</template>
+
+<script>
+import JTableRow from '@mps/mps-j-table-row/MPSJTableRow'
+export default {
+  name: 'JTable',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    JTableRow
+  }
+}
+</script>
